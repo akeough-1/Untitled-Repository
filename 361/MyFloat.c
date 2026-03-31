2af9ad3d005583a3748734b829b9dbe5888d82f8
akeough-1 <ack8424@gmail.com>
2026-03-31 00:41:43 -0500

    converted all inputs

--- 361/MyFloat.c
+++ 361/MyFloat.c
@@ -190,30 +190,77 @@ int main(int argc, char **argv) {
     /* define the number of chars in input array */
     int str_len = j;
 
+    /*
     printf("DEBUG: dec loc = %d\n",dec_ind);
     printf("DEBUG: exp loc = %d\n",exp_ind);
     printf("DEBUG: str len = %d\n",str_len);
+    */
 
     /* if exp_ind still -1, then there is none */
     int exp_val;
     if (exp_ind == -1) {
+        /* set this to the string length for when it's compared to later */
         exp_ind = str_len;
         exp_val = 0;
     }
+
+    /* otherwise convert the exp array to an int */
     else {
         int size = str_len - 1 - exp_ind;
         char *exp_str = malloc(size);
         for (i=0 ; i<size ; i++) {
-            /* get the right index from the input str (account for -)*/
+            /* get the right index from the input str (account for - and e)*/
             j = exp_ind + 1 + i + str_start;
             exp_str[i] = argv[3][j];
         }
         exp_val = atoi(exp_str);
         free(exp_str);
     }
+    printf("DEBUG: exp val = %d\n",exp_val);
 
-    /* extract integer component */
-    
+    int int_val;
+    double frac_val;
+
+    /* if dec_ind still -1, then no fraction component */
+    if (dec_ind == -1) {
+        frac_val = 0;
+        
+        int size = exp_ind;
+        char *int_str = malloc(size);
+        for (i=0 ; i<size ; i++) {
+            j = str_start + i;
+            int_str[i] = argv[3][j];
+        }
+        int_val = atoi(int_str);
+        free(int_str);
+    }
+
+    /* otherwise there definitely is a fraction component */
+    else {
+        int int_size = dec_ind;
+        char *int_str = malloc(int_size);
+        for (i=0 ; i<int_size ; i++) {
+            j = str_start + i;
+            int_str[i] = argv[3][j];
+        }
+        int_val = atoi(int_str);
+        free(int_str);
+
+        int frac_size = exp_ind - dec_ind - 1;
+        char *frac_str = malloc(frac_size+2);
+        frac_str[0] = '0';
+        frac_str[1] = '.';
+        for (i=0 ; i<frac_size ; i++) {
+            j = str_start + dec_ind + 1 + i;
+            frac_str[i+2] = argv[3][j];
+            /* printf("DEBUG: crnt char = %c\n",frac_str[i+2]); */
+        }
+        frac_val = atof(frac_str);
+        free(frac_str);
+    }
+
+    printf("DEBUG: int val = %d\n",int_val);
+    printf("DEBUG: frac val = %lf\n",frac_val);
 
     return 0;
 }
