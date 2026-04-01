b4be00778c0ff762fa0d843c21819a6231d2d08e
akeough-1 <ack8424@gmail.com>
2026-04-01 16:18:06 -0500

    wrote _unit_parser function

--- Units Project/Units.py
+++ Units Project/Units.py
@@ -1,4 +1,11 @@
 class Dimension:
+    compound_units = {
+        "N":["kg","m","-s","-s"],
+        "lbf":["slug","ft","-s","-s"],
+        "J":["N","m"],
+        "Pa":["N","-m","-m"],
+        "atm":["101325*Pa"]
+        } 
     length_units = {
         "m":1,
         "mm":1e-3,
@@ -7,10 +14,28 @@ class Dimension:
         "ft":0.3048,
         "in":2.54/100,
     }
+    mass_units = {
+        "kg":1,
+        "g":1e-3,
+        "slug":14.59390,
+        "lbm":14.59390/32.174
+    }   
+    temp_units = {
+        "K":1,
+        "R":5/9,
+    }
+    time_units = {
+        "s":1,
+        "ms":1e-3,
+        "min":60,
+        "hr":3600,
+        "hrs":3600,
+    }
 
     def __init__(self, magnitude:int | float, units:str):
         self.magnitude = magnitude
 
+        # set all initial powers to 0
         self.fund_units = {
             "Length":0,
             "Mass":0,
@@ -35,7 +60,71 @@ class Dimension:
         self._unit_parser(units)
 
     def _unit_parser(self, unit_str:str):
-        pass
+        # loop until there are no more * or / separators
+        end = False; skip = False
+        while(not end):
+            if unit_str[0] == '*':
+                pow = 1
+                unit_str = unit_str[1:]
+            elif unit_str[0] == '/':
+                pow = -1
+                unit_str = unit_str[1:]
+            else:
+                pow = 1
+
+            # loop through every char in string
+            for i in range(len(unit_str)):
+                # check if at the end of the string
+                if i == len(unit_str) - 1:
+                    i+=1
+                    end = True
+                    break
+
+                # check if in the middle of "**"
+                elif skip == True:
+                    skip = False
+
+                elif unit_str[i] == '*':
+                    if unit_str[i+1] == '*':
+                        # need to check if this is actually an exp
+                        skip = True
+                    else:
+                        break
+
+                elif unit_str[i] == '/':
+                    break
+
+            # separate into current and everything after (not including sep)
+            crnt_str = unit_str[:i]
+            unit_str = unit_str[i:]
+            #print(f"DEBUG: crnt str = {crnt_str}, unit str = {unit_str}")
+
+            # separate current string into label and power
+            # truncate string if it has an exponent
+            for i in range(len(crnt_str)):
+                if crnt_str[i] == '*' and crnt_str[i+1] == '*':
+                    try:
+                        pow = pow*int(crnt_str[i+2:])
+                    except ValueError:
+                        raise TypeError(f"Could not convert exponent in {crnt_str} to int.")
+                    
+                    crnt_str = crnt_str[:i]
+                    break
+
+                elif crnt_str[i] == '^':
+                    try:
+                        pow = pow*int(crnt_str[i+1:])
+                    except ValueError:
+                        raise TypeError(f"Could not convert exponent in \"{crnt_str}\" to int.")
+
+                    crnt_str = crnt_str[:i]
+                    break
+
+            if pow == 0:
+                raise ValueError(f"Exponent of \"{crnt_str}\" should not be 0.")
+
+            #print(f"DEBUG: crnt str = {crnt_str}, pow = {pow}")
+            self._unit_decoder(crnt_str, pow)
 
     def _unit_decoder(self, unit_str:str,pow:int):
         pass
@@ -161,6 +250,4 @@ class Dimension:
     def __round__(self, other):
         pass
 
-dim1 = Dimension(32,"F")
-print(dim1.magnitude)
-print(dim1.fund_units)
+dim1 = Dimension(32,"kg**2*m^4/s**3")
